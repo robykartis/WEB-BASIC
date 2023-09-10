f65b53231dec354be3f6e81d57ddac9bd79fe7e8
Roby Karti S <55522784+robykartis@users.noreply.github.com>
2023-09-11 01:37:22 +0700

    Lanjut Kantor

---
+++ basic-next-js/app/(home-route)/page.tsx
@@ -0,0 +1,11 @@
+import { Button } from "@/components/ui/button";
+
+
+export default function Home() {
+  return (
+    <main>
+      <h1>Home</h1>
+      <Button>Button</Button>
+    </main>
+  )
+}
