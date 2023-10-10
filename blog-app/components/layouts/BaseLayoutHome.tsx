c072de8ec9d8e3c752e792fb58a3b724a0c63cf0
robykartis <robykarti.s@gmail.com>
2023-10-10 15:55:17 +0700

    Home Clar

---
+++ blog-app/components/layouts/BaseLayoutHome.tsx
@@ -0,0 +1,21 @@
+'use client'
+import React from 'react'
+import NavbarHome from './NavbarHome'
+import FooterHome from './FooterHome'
+import { DarkThemeToggle, Flowbite } from 'flowbite-react';
+
+export default function BaseLayoutHome({
+    children,
+}: {
+    children: React.ReactNode
+}) {
+    return (
+        <>
+            <Flowbite>
+                <NavbarHome />
+                {children}
+                <FooterHome />
+            </Flowbite>
+        </>
+    )
+}
---
+++ blog-app/components/layouts/NavbarHome.tsx
@@ -0,0 +1,55 @@
+'use client';
+
+import { Button, Navbar } from 'flowbite-react';
+import { DarkThemeToggle } from 'flowbite-react';
+
+export default function NavbarHome() {
+    return (
+        <Navbar
+            fluid
+            className='border-b sticky backdrop-blur-lg bg-white/75 top-0 z-50'
+        >
+            <Navbar.Brand href="https://flowbite-react.com">
+                <img
+                    alt="MTI Logo"
+                    className="mr-3 h-6 sm:h-9"
+                    src="assets/logo/logo.png"
+                />
+                <span className="self-center whitespace-nowrap text-xl font-semibold dark:text-white">
+                    MTI
+                </span>
+            </Navbar.Brand>
+            <div className="flex md:order-2 gap-2">
+                <Button>
+                    Get started
+                </Button>
+                <Navbar.Toggle />
+                <DarkThemeToggle />
+            </div>
+            <Navbar.Collapse>
+                <Navbar.Link
+                    active
+                    href="#"
+                >
+                    <p>
+                        Home
+                    </p>
+                </Navbar.Link>
+                <Navbar.Link href="#">
+                    About
+                </Navbar.Link>
+                <Navbar.Link href="#">
+                    Services
+                </Navbar.Link>
+                <Navbar.Link href="#">
+                    Pricing
+                </Navbar.Link>
+                <Navbar.Link href="#">
+                    Contact
+                </Navbar.Link>
+            </Navbar.Collapse>
+        </Navbar>
+    )
+}
+
+
